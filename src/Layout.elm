db870cfef0a68cc3317fdf15b0778f9d70d912fa
Johann-Goncalves-Pereira <johann.gon.pereira@gmail.com>
2022-08-27 22:13:16 -0300

    feat: start

--- src/Layout.elm
+++ src/Layout.elm
@@ -13,8 +13,8 @@ import Regex
 
 type alias Model msg =
     { route : Route
-    , mainContent : List (Html msg)
-    , mainAttrs : List (Attribute msg)
+    , rootContent : List (Html msg)
+    , rootAttrs : List (Attribute msg)
     }
 
 
@@ -28,8 +28,8 @@ type alias Link =
 initLayout : Model msg
 initLayout =
     { route = Route.Home_
-    , mainContent = []
-    , mainAttrs = []
+    , rootContent = []
+    , rootAttrs = []
     }
 
 
@@ -43,16 +43,6 @@ defaultLink =
 
 
 -- Structure
-
-
-isRoute : Route -> Route -> Bool
-isRoute route compare =
-    case ( route, compare ) of
-        ( Route.Home_, Route.Home_ ) ->
-            True
-
-        _ ->
-            False
 
 
 routeName : Route -> String
@@ -101,81 +91,24 @@ classBuilder string =
 viewLayout : Model msg -> List (Html msg)
 viewLayout model =
     let
-        mainClass : Attribute msg
-        mainClass =
-            class <| "root__main main--" ++ classBuilder (routeName model.route)
+        pageClass : String
+        pageClass =
+            "root--"
+                ++ classBuilder
+                    (routeName model.route)
+
+        rootClass : String
+        rootClass =
+            "root"
     in
-    [ div
-        [ id "root"
-        , placeholderStyles 0
-        , classList
-            [ ( "root", True )
-            , ( "root--" ++ classBuilder (routeName model.route), True )
+    div
+        ([ id rootClass
+         , classList
+            [ ( rootClass, True )
+            , ( pageClass, True )
             ]
-        ]
-        [ viewHeader model
-        , main_ (mainClass :: model.mainAttrs) model.mainContent
-        ]
-    ]
-
-
-viewHeader : Model msg -> Html msg
-viewHeader model =
-    header [ class "root__header" ]
-        [ viewHeaderLinks model [ Route.Home_, Route.About ]
-            |> nav
-                [ class "root__header__nav"
-                , placeholderStyles 1
-                ]
-        ]
-
-
-viewHeaderLinks : Model msg -> List Route -> List (Html msg)
-viewHeaderLinks model links =
-    List.map
-        (\staticRoute ->
-            viewLink
-                { defaultLink
-                    | routeName = routeName staticRoute
-                    , routeStatic = staticRoute
-                    , routeReceived = model.route
-                }
+         ]
+            ++ model.rootAttrs
         )
-        links
-
-
-viewLink : Link -> Html msg
-viewLink model =
-    a
-        [ classList
-            [ ( "root__header__links", True )
-            , ( "root__header__links--current-page"
-              , isRoute model.routeReceived model.routeStatic
-              )
-            ]
-        , placeholderStyles 2
-        , href <| Route.toHref model.routeStatic
-        , tabindex 0
-        ]
-        [ text model.routeName ]
-
-
-placeholderStyles : Int -> Attribute msg
-placeholderStyles index =
-    let
-        listOfStyles : List (Attribute msg)
-        listOfStyles =
-            [ class "grid grid-rows-[auto,1fr] gap-8 h-full"
-            , class "flex justify-center gap-4 text-2 bg-surface-200 shadow-inner"
-            , class "p-4 font-semibold md:p-8"
-            ]
-
-        arrayOfStyles : Array.Array (Attribute msg)
-        arrayOfStyles =
-            Array.fromList listOfStyles
-
-        getStyle : Maybe (Attribute msg)
-        getStyle =
-            Array.get index arrayOfStyles
-    in
-    Maybe.withDefault (class "error") getStyle
+        model.rootContent
+        |> List.singleton
